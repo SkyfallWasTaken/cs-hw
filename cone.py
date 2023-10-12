80b097a5362323ba94705c4bb723db5a46cf8789
SkyfallWasTaken <mahadkalam1234@gmail.com>
2023-10-12 20:28:17 +0000

    Stuff

---
+++ cone.py
@@ -0,0 +1,2 @@
+def coneVolume(radius, height):
+    return math.pi * (radius ** 2) * (height / 3)
--- dog.py
+++ dog.py
@@ -1,5 +1,5 @@
 dogage = float(input("Dog age"))
 if age >= 2:
-    print(age * 12, "is the human age")
+    print(dogage * 12, "is the human age")
 else:
-    print(24 + (age - 2) * 6, "is the human age")
+    print(24 + (dogage - 2) * 6, "is the human age")
