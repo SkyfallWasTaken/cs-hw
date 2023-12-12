4f79721d02cf69be323b9d82a60b0c4cfc8092b7
firecoder1 <github@skyfall.33mail.com>
2023-12-12 21:39:03 +0000

    Fix new student

--- new_student_username.py
+++ new_student_username.py
@@ -2,4 +2,4 @@ year = input("Please enter your year of entry: ")
 first_name = input("Please enter your first name: ")
 surname = input("Please enter your surname: ")
 
-print("Your username is:", f"{year[2:4]}{surname[:4]}{first_name[:3]}")
+print("Your username is:", year[2:4] + surname[:4] + first_name[:3])
